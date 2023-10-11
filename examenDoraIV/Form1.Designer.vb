43674ef5151b5f1503ff6af49e20c091169ca69c
Dora Maribel de Leon <Dora Maribel de Leon@DESKTOP-I37108E>
2023-10-11 16:03:09 -0600

    Agregar archivos de proyecto.

---
+++ examenDoraIV/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(368, 107)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(33, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "hola "
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(270, 143)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(229, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "examen final Dora De Leon"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(338, 189)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 2
+        Button2.Text = "1"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
